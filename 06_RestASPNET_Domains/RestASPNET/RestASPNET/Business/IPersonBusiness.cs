897233375a123cd26acf0bea40d4c8a6f9bbea97
Fts99 <47905996+Fts99@users.noreply.github.com>
2021-04-17 14:27:29 -0300

    Working with separates domains

---
+++ 06_RestASPNET_Domains/RestASPNET/RestASPNET/Business/IPersonBusiness.cs
@@ -0,0 +1,19 @@
+﻿using RestASPNET.Model;
+using System.Collections.Generic;
+
+namespace RestASPNET.Business
+{
+    public interface IPersonBusiness
+    {
+        Person Create(Person person);
+
+        Person FindById(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        bool Delete(long id);
+
+    }
+}
---
+++ 06_RestASPNET_Domains/RestASPNET/RestASPNET/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -0,0 +1,78 @@
+﻿using RestASPNET.Model;
+using RestASPNET.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestASPNET.Repository.Implementations
+{
+    public class PersonRepositoryImplementation : IPersonRepository
+    {
+        private MySQLContext _mySqlContext;
+
+        public PersonRepositoryImplementation(MySQLContext mySQLContext) 
+        {
+            _mySqlContext = mySQLContext;
+        }
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _mySqlContext.Add(person);
+                _mySqlContext.SaveChanges();
+            }
+            catch (Exception )
+            {
+                throw;
+            }
+            return person;
+        }
+
+        public bool Delete(long id)
+        {
+            if (Exists(id))
+            {
+                var person = _mySqlContext.Persons.Find(id);
+                _mySqlContext.Persons.Remove(person);
+                _mySqlContext.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public List<Person> FindAll()
+        {
+            return _mySqlContext.Persons.ToList();
+        }
+
+        public Person FindById(long id)
+        {
+            return _mySqlContext.Persons.Find(id);
+        }
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id))
+            {
+                return null;
+            }
+
+            try
+            {
+                _mySqlContext.Persons.Update(person);
+                _mySqlContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return person;
+        }
+
+        public bool Exists(long id)
+        {
+            return _mySqlContext.Persons.Any(x => x.Id == id);
+        }
+    }
+}
