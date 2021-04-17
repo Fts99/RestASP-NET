84ec3b9db6156818f6b61653c135f8865a7bef15
Fts99 <47905996+Fts99@users.noreply.github.com>
2021-04-17 14:26:21 -0300

    API Versioning

---
+++ 05_RestASPNET_MVC_Versioning/RestASPNET/RestASPNET/Controllers/PersonController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestASPNET.Model;
+using RestASPNET.Services.Implementations;
+
+namespace RestASPNET.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            Person person = _personService.FindById(id);
+            return (person == null) ? NotFound() : Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            Person createdPerson = _personService.Create(person);
+            return (createdPerson == null) ? BadRequest() : Ok(createdPerson);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            Person UpdatedPerson = _personService.Update(person);
+            return (UpdatedPerson == null) ? BadRequest() : Ok(UpdatedPerson);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
