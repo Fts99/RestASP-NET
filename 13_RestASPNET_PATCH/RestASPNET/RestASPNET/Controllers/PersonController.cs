a39825dd4c65683a478eedb4ea29f47c3deba546
Fts99 <47905996+Fts99@users.noreply.github.com>
2021-08-14 19:36:52 -0300

    Finshing authentication

---
+++ 13_RestASPNET_PATCH/RestASPNET/RestASPNET/Controllers/PersonController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestASPNET.Business;
+using RestASPNET.Data.VO;
+using RestASPNET.Hypermedia.Filters;
+using System.Collections.Generic;
+
+namespace RestASPNET.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Authorize("Bearer")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+        private IPersonBusiness _personBusiness;
+
+        public PersonController(ILogger<PersonController> logger, IPersonBusiness personBusiness)
+        {
+            _logger = logger;
+            _personBusiness = personBusiness;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200,Type = typeof(List<PersonVO>))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_personBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(PersonVO))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var person = _personBusiness.FindById(id);
+            return (person == null) ? NotFound() : Ok(person);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200, Type = typeof(PersonVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] PersonVO person)
+        {
+            var createdPerson = _personBusiness.Create(person);
+            return (createdPerson == null) ? BadRequest() : Ok(createdPerson);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(200, Type = typeof(PersonVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] PersonVO person)
+        {
+            var UpdatedPerson = _personBusiness.Update(person);
+            return (UpdatedPerson == null) ? BadRequest() : Ok(UpdatedPerson);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public IActionResult Delete(long id)
+        {
+            return _personBusiness.Delete(id) ? Ok() : NotFound();
+            
+        }
+    }
+}
---
+++ 13_RestASPNET_PATCH/RestASPNET/RestASPNET/Repository/Generic/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using RestASPNET.Model.Base;
+using System.Collections.Generic;
+
+namespace RestASPNET.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+
+        T FindById(long id);
+
+        List<T> FindAll();
+
+        T Update(T item);
+
+        bool Delete(long id);
+
+        bool Exists(long id);
+
+    }
+}
